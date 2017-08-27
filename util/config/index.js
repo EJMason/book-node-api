888434c19eff6031a862c91ffbbc4a0a583931c5
EJMason <eliotjunior@gmail.com>
2017-08-26 23:55:50 -0700

    [prog] config settings added for server

--- util/config/index.js
+++ util/config/index.js
@@ -1,4 +1,10 @@
 
-export default {
+module.exports = {
+  nodeEnv: process.env.NODE_ENV,
+  isProd: process.env.NODE_ENV === 'production',
+  PORT: process.env.PORT,
 
-};
+  db: {
+    url: ""
+  }
+}
